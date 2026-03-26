36a6a79dc401a0fb3ca9d8d76707718a0c248876
Estimé G.M. KAO <estimekao77@gmail.com>
2026-03-26 18:28:57 +0000

    Suppression des using inutiles dans Module.cs
    Les directives using non utilisées ont été retirées du fichier Module.cs afin d'alléger le code et d'éviter les dépendances superflues.

--- src/Domain/Entities/Cours/Module.cs
+++ src/Domain/Entities/Cours/Module.cs
@@ -1,10 +1,6 @@
 ﻿using BlazorHero.CleanArchitecture.Domain.Contracts;
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlazorHero.CleanArchitecture.Domain.Entities.Cours
 {
